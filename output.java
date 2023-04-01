6902f86ad8d60f76a30c3e972b2ac5a5a361dc62
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-04-01 12:15:17 +0530

    Create output.java

---
+++ output.java
@@ -0,0 +1,5 @@
+Product         Sales_2021      Sales_2022      Variance        Variance_Percentage
+Coffee          250             230             -20             -8%
+Green_Tea       50              60              10              20%
+Orange_Soda     180             210             30              16.67%
+Lemon_Soda      220             205             -15             -6.82%
